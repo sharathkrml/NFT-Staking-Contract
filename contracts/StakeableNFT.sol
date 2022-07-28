cf1a40850fea96400b169101dc0b0febe780c3ed
Sharath <sharathkrml@gmail.com>
2022-07-28 12:04:22 +0530

    added finding length

--- contracts/StakeableNFT.sol
+++ contracts/StakeableNFT.sol
@@ -2,23 +2,38 @@
 pragma solidity ^0.8.4;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
+import 'hardhat/console.sol';
 
 contract StakeableNFT is ERC721 {
-    uint256 private tokenId;
+    using Strings for uint256;
+    uint256 private s_tokenId;
+    string private s_baseUri;
 
-    constructor() ERC721('StakeableNFT', 'STK') {}
-
-    function _baseURI() internal pure override returns (string memory) {
-        return 'https://www.google.com';
+    constructor(string memory _baseURI) ERC721('StakeableNFT', 'STK') {
+        s_baseUri = _baseURI;
     }
 
     function safeMint(address to) public {
-        tokenId++;
-        _safeMint(to, tokenId);
+        console.log(bytes(s_baseUri).length);
+        s_tokenId++;
+        _safeMint(to, s_tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        _requireMinted(tokenId);
+
+        return string(abi.encodePacked(s_baseUri, tokenId.toString(), '.json'));
     }
 
     // getter functions
     function getTokenId() external view returns (uint256) {
-        return tokenId;
+        return s_tokenId;
     }
 }
