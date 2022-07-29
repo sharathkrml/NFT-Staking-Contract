63608bf808ff061e8fc030eae49f96693a5b5fe5
Sharath <sharathkrml@gmail.com>
2022-07-29 17:36:24 +0530

    added comments

--- contracts/NFTStaking.sol
+++ contracts/NFTStaking.sol
@@ -19,7 +19,7 @@ contract NFTStaking is IERC721Receiver, ReentrancyGuard {
     struct Stake {
         uint256 lastTimeStamp; // store added time / last claimed time
         uint256 emissionRate;
-        uint256 tokenAmount;
+        uint256 tokenQuantity;
     }
 
     mapping(address => Stake) private s_stakeByAddress;
@@ -27,13 +27,13 @@ contract NFTStaking is IERC721Receiver, ReentrancyGuard {
     event Staked(
         uint256 lastTimeStamp,
         uint256 emissionRate,
-        uint256 tokenAmount,
+        uint256 tokenQuantity,
         uint256[] tokenIds
     );
     event Unstaked(
         uint256 lastTimeStamp,
         uint256 emissionRate,
-        uint256 tokenAmount,
+        uint256 tokenQuantity,
         uint256[] tokenIds
     );
     event Claim(uint256 amount);
@@ -48,42 +48,64 @@ contract NFTStaking is IERC721Receiver, ReentrancyGuard {
         s_emissionPerDay = _perDay * 1 ether;
     }
 
+    /**
+     * function to stake given NFTs
+     * @dev takes in NFT ids,if it's not the first time user staking,
+     * updates the tokenQuantity according to last staking
+     * add ids to mapping s_ownerOf[id]
+     * increment emissionRate according with no.of NFTs
+     * transfers NFT to Staking contract
+     * @param _ids takes in array of tokenIds
+     */
     function stake(uint256[] calldata _ids) external nonReentrant {
         Stake memory m_stake = s_stakeByAddress[msg.sender];
+        // if it is first time,no need to calculate tokenQuantity
         if (m_stake.lastTimeStamp != 0) {
-            m_stake.tokenAmount = _tokenAmountSoFar(
+            m_stake.tokenQuantity = _tokenQuantitySoFar(
                 m_stake.lastTimeStamp,
                 m_stake.emissionRate,
-                m_stake.tokenAmount
+                m_stake.tokenQuantity
             );
         }
         m_stake.lastTimeStamp = block.timestamp;
         for (uint256 i = 0; i < _ids.length; i++) {
             if (i_nft.ownerOf(_ids[i]) != msg.sender) {
+                // confirms if all tokens belong to msg.sender
                 revert NFTStaking__NotOwner(_ids[i]);
             }
+            // add ids to mapping
             s_ownerOf[_ids[i]] = msg.sender;
+            // emission rate incremented according with no of NFTs
             m_stake.emissionRate += s_emissionPerDay;
+            // transfers the NFT to contract
             i_nft.safeTransferFrom(msg.sender, address(this), _ids[i]);
         }
         s_stakeByAddress[msg.sender] = m_stake;
         emit Staked(
             m_stake.lastTimeStamp,
             m_stake.emissionRate,
-            m_stake.tokenAmount,
+            m_stake.tokenQuantity,
             _ids
         );
     }
 
+    /**
+     * used to unclaim given NFTs
+     * @dev sets up tokenQuantity & lastTimeStamp first
+     * verifies if id belonged to msg.sender using s_ownerOf mapping
+     * reduces emissionRate for further calculations
+     * transfers NFTs to owner
+     * @param _ids takes in list of NFT ids
+     */
     function unstake(uint256[] calldata _ids) external nonReentrant {
         Stake memory m_stake = s_stakeByAddress[msg.sender];
         if (m_stake.lastTimeStamp == 0) {
             revert NFTStaking__NothingStaked();
         }
-        m_stake.tokenAmount = _tokenAmountSoFar(
+        m_stake.tokenQuantity = _tokenQuantitySoFar(
             m_stake.lastTimeStamp,
             m_stake.emissionRate,
-            m_stake.tokenAmount
+            m_stake.tokenQuantity
         );
         m_stake.lastTimeStamp = block.timestamp;
         for (uint256 i = 0; i < _ids.length; i++) {
@@ -97,56 +119,78 @@ contract NFTStaking is IERC721Receiver, ReentrancyGuard {
         emit Unstaked(
             m_stake.lastTimeStamp,
             m_stake.emissionRate,
-            m_stake.tokenAmount,
+            m_stake.tokenQuantity,
             _ids
         );
     }
+
+    /**
+     * function used to claim tokens
+     * @dev calculate the tokenQuantity & lastTimeStamp
+     * reduces amount from calculated tokenQuantity & mints the amount to msg.sender
+     * @param _amount takes in the user specified amount to mint
+     */
 
     function claim(uint256 _amount) external nonReentrant {
         Stake memory m_stake = s_stakeByAddress[msg.sender];
         if (m_stake.lastTimeStamp == 0) {
             revert NFTStaking__NothingStaked();
         }
-        m_stake.tokenAmount = _tokenAmountSoFar(
+        m_stake.tokenQuantity = _tokenQuantitySoFar(
             m_stake.lastTimeStamp,
             m_stake.emissionRate,
-            m_stake.tokenAmount
+            m_stake.tokenQuantity
         );
         m_stake.lastTimeStamp = block.timestamp;
-        if (m_stake.tokenAmount < _amount) {
+        if (m_stake.tokenQuantity < _amount) {
             revert NFTStaking__NotEligibleForThisMuch();
         }
-        m_stake.tokenAmount -= _amount;
+        m_stake.tokenQuantity -= _amount;
         s_stakeByAddress[msg.sender] = m_stake;
         i_rewardToken.mint(msg.sender, _amount);
         emit Claim(_amount);
     }
+
+    /**
+     * used to claim all tokens deserving to msg.sender
+     * @dev first calculates the tokenQuantity & saves it to _amount variable
+     * sets the new timeStamp & tokenQuantity as zero(as we are gonna mint all)
+     */
 
     function claimAll() external nonReentrant {
         Stake memory m_stake = s_stakeByAddress[msg.sender];
-        uint256 _amount;
-        if (m_stake.lastTimeStamp != 0) {
-            _amount = _tokenAmountSoFar(
-                m_stake.lastTimeStamp,
-                m_stake.emissionRate,
-                m_stake.tokenAmount
-            );
-            m_stake.lastTimeStamp = block.timestamp;
+        if (m_stake.lastTimeStamp == 0) {
+            revert NFTStaking__NothingStaked();
         }
-        m_stake.tokenAmount = 0;
+        uint256 _amount = _tokenQuantitySoFar(
+            m_stake.lastTimeStamp,
+            m_stake.emissionRate,
+            m_stake.tokenQuantity
+        );
+        m_stake.lastTimeStamp = block.timestamp;
+        m_stake.tokenQuantity = 0;
         s_stakeByAddress[msg.sender] = m_stake;
         i_rewardToken.mint(msg.sender, _amount);
         emit Claim(_amount);
     }
 
-    function _tokenAmountSoFar(
+    /**
+     * function used to calculate current tokenQuantity
+     * @dev takes in (block.timestamp-lastTimeStamp) gives out seconds passed from prev calculation
+     * multiplying it by emissionRatePerDay / 1 days gives out totalAmount from last calculation
+     * adding that to currtokenQuantity gives updated tokenQuantity
+     * @param lastTimeStamp takes in lastTime tokenQuantity is calculated
+     * @param emissionRatePerDay takes in emission rate of specific stake  per day
+     * @param currtokenQuantity takes quantity after last calculation
+     */
+    function _tokenQuantitySoFar(
         uint256 lastTimeStamp,
-        uint256 emissionRate,
-        uint256 currTokenAmount
+        uint256 emissionRatePerDay,
+        uint256 currtokenQuantity
     ) internal view returns (uint256) {
         return
-            currTokenAmount +
-            (((block.timestamp - lastTimeStamp) * emissionRate) / 1 days);
+            currtokenQuantity +
+            (((block.timestamp - lastTimeStamp) * emissionRatePerDay) / 1 days);
     }
 
     /**
